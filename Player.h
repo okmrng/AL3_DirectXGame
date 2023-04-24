c4dd8dd70e13a915985097674b2158a45573b56d
奥村　凪 <k022g1055@g.neec.ac.jp>
2023-04-24 14:11:24 +0900

    02_01_確認課題

---
+++ Player.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "Model.h"
+#include "WorldTransform.h"
+
+/// <summary>
+/// 自キャラ
+/// </summary>
+class Player {
+	//メンバ関数
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="model">モデル</param>
+	/// <param name="textureHandle">テクスチャハンドル</param>
+	void Initialize(Model* model,uint32_t textureHandle);
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	/// <param name="viewProjection">ビュープロジェクション（参照渡し）</param>
+	void Draw(ViewProjection& viewProjection);
+
+	//メンバ変数
+private:
+	//ワールド変換データ
+	WorldTransform worldTransform_;
+
+	//モデル
+	Model* model_ = nullptr;
+
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+};
--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -4,16 +4,38 @@
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { 
+	delete model_;
+
+	//自キャラの解放
+	delete player_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+
+	//テクスチャの読み込み
+	textureHandle_ = TextureManager::Load("sample.png");
+
+	//3Dモデルの生成
+	model_ = Model::Create();
+
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+
+	//自キャラの生成
+	player_ = new Player();
+	//自キャラの初期化
+	player_->Initialize(model_, textureHandle_);
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	//自キャラの更新
+	player_->Update();
+}
 
 void GameScene::Draw() {
 
@@ -41,6 +63,9 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+
+	//自キャラの描画
+	player_->Draw(viewProjection_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
