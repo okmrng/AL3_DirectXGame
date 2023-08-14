30ec40b24101231c2d8deca4fd39443e6fece2ca
奥村　凪 <k022g1055@g.neec.ac.jp>
2023-08-14 20:19:34 +0900

    動く敵のクラスを作成

---
+++ EnemyMove.cpp
@@ -0,0 +1,127 @@
+#include "EnemyMove.h"
+
+#include "GameScene.h"
+#include "Player.h"
+
+void EnemyMove::Initialize(Model* model, const Vector3& position, const Vector3& velocity) {
+	// NULLポインタチェック
+	assert(model);
+	model_ = model;
+	// テクスチャ読み込み
+	textureHandle_ = TextureManager::Load("model/enemy.png");
+
+	// ワールド変換の初期化
+	worldTransform_.Initialize();
+	// 引数で受け取った初期座標をセット
+	worldTransform_.translation_ = position;
+
+	// 速度
+	velocity_ = velocity;
+
+	// 接近フェーズ初期化
+	ApproachInitialize();
+}
+
+void EnemyMove::Update() {
+	// フェーズ
+	switch (phase_) {
+	case Phase::Approach:
+	default:
+		// 移動
+		ApproachUpdate();
+		break;
+	case Phase::Leave:
+		LeaveUpdate();
+		break;
+	}
+
+	// HPが0になったらデス
+	if (HP <= 0) {
+		isDead_ = true;
+	}
+
+	worldTransform_.UpdateMatrix();
+}
+
+void EnemyMove::Draw(const ViewProjection& viewProjection) {
+	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+}
+
+void EnemyMove::ApproachInitialize() {
+	// 発射タイマーを初期化
+	kFireTimer = kFireInterval;
+}
+
+void EnemyMove::ApproachUpdate() {
+	// 座標を移動させる
+	worldTransform_.translation_.z += velocity_.z;
+
+	// 規定の位置に到達したら離脱
+	if (worldTransform_.translation_.z < 13.0f) {
+		phase_ = Phase::Leave;
+	}
+
+	// 発射タイマーカウントダウン
+	--kFireTimer;
+	if (kFireTimer <= 0) {
+		// 弾を発射
+		Fire();
+		// 発射タイマーを初期化
+		kFireTimer = kFireInterval;
+	}
+}
+
+void EnemyMove::LeaveUpdate() {
+	if (deathTimer_-- < 0) {
+		isDead_ = true;
+	}
+
+	// 座標を移動させる(1フレーム分の移動量を足しこむ)
+	worldTransform_.translation_.x += velocity_.x;
+	worldTransform_.translation_.y += velocity_.y;
+	worldTransform_.translation_.z += velocity_.z;
+}
+
+void EnemyMove::Fire() {
+	assert(player_);
+
+	// 弾の速度
+	const float kBulletSpeed = -0.5f;
+
+	Vector3 playerWorldPos = player_->GetWorldPositiopn(); // 自キャラのワールド座標
+	Vector3 enemyWorldPos = GetWorldPositiopn();           // 敵キャラのワールド座標
+	// 敵キャラと自キャラの差分ベクトルを求める
+	Vector3 velocity;
+	velocity.x = enemyWorldPos.x - playerWorldPos.x;
+	velocity.y = enemyWorldPos.y - playerWorldPos.y;
+	velocity.z = enemyWorldPos.z - playerWorldPos.z;
+	// 正規化
+	velocity = Normalize(velocity);
+	// 長さを速さに合わせる
+	velocity.x *= kBulletSpeed;
+	velocity.y *= kBulletSpeed;
+	velocity.z *= kBulletSpeed;
+
+	// 弾を生成し初期化
+	EnemyBullet* newBullet = new EnemyBullet();
+	newBullet->Initialize(model_, worldTransform_.translation_, velocity);
+
+	// 弾を登録する
+	gameScene_->AddEnemyBullet(newBullet);
+}
+
+void EnemyMove::OnColision() { --HP; }
+
+void EnemyMove::OnBombCollision() { isDead_ = true; }
+
+Vector3 EnemyMove::GetWorldPositiopn() {
+	// ワールド座標を入れる変数
+	Vector3 worldPos;
+
+	// ワールド行列の平行移動成分を取得(ワールド座標)
+	worldPos.x = worldTransform_.translation_.x;
+	worldPos.y = worldTransform_.translation_.y;
+	worldPos.z = worldTransform_.translation_.z;
+
+	return worldPos;
+}
---
+++ EnemyMove.h
@@ -0,0 +1,122 @@
+#pragma once
+
+#include "EnemyBullet.h"
+#include "Material.h"
+#include "Model.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+#include <cassert>
+#include <cmath>
+#include <list>
+
+// 前方宣言
+class Player;
+class GameScene;
+
+/// <summary>
+/// 動く敵
+/// </summary>
+class EnemyMove {
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="model">モデル</param>
+	/// <param name="position">初期座標</param>
+	void Initialize(Model* model, const Vector3& position, const Vector3& velocity);
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	/// <param name="viewProjection">ビュープロジェクション</param>
+	void Draw(const ViewProjection& viewProjection);
+
+	/// <summary>
+	/// 接近フェーズの初期化関数
+	/// </summary>
+	void ApproachInitialize();
+
+	/// <summary>
+	/// 接近フェーズの更新関数
+	/// </summary>
+	void ApproachUpdate();
+
+	/// <summary>
+	/// 離脱フェーズの更新関数
+	/// </summary>
+	void LeaveUpdate();
+
+	/// <summary>
+	/// 攻撃
+	/// </summary>
+	void Fire();
+
+	/// <summary>
+	/// 衝突を検出したら呼び出されるコールバック関数
+	/// </summary>
+	void OnColision();
+
+	/// <summary>
+	/// ボムとの衝突処理
+	/// </summary>
+	void OnBombCollision();
+
+	// セッター
+	void SetPlayer(Player* player) { player_ = player; }
+
+	void SetGameScene(GameScene* gameScene) { gameScene_ = gameScene; }
+
+	// ゲッター
+	/// <summary>
+	/// ワールド座標を取得
+	/// </summary>
+	/// <returns></returns>
+	Vector3 GetWorldPositiopn();
+
+	/// <summary>
+	/// 半径取得
+	/// </summary>
+	/// <returns></returns>
+	const float GetRadius() { return radius_; }
+
+	bool GetIsDead() { return isDead_; }
+
+	static const int kFireInterval = 60; // 発射間隔
+
+private:
+	int32_t kFireTimer = 0; // 発射タイマー
+
+	WorldTransform worldTransform_; // ワールド変換データ
+
+	// モデル
+	Model* model_ = nullptr;
+	uint32_t textureHandle_ = 0u; // テクスチャハンドル
+
+	Vector3 velocity_; // 速度
+
+	// フェーズ
+	// 行動フェーズ
+	enum class Phase {
+		Approach, // 接近する
+		Leave     // 離脱する
+	};
+	Phase phase_ = Phase::Approach;
+	int32_t toLeaveTimer = 60 * 3; // 離脱フェーズに移るまでの時間
+
+	Player* player_ = nullptr; // 自キャラ
+
+	const float radius_ = 2.0f; // 半径
+
+	GameScene* gameScene_ = nullptr; // ゲームシーン
+
+	// デス
+	int32_t deathTimer_ = 300; // デスタイマー
+	bool isDead_ = false;      // デスフラグ
+
+	int32_t HP = 5;
+};
