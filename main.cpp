2b7bee7c8f3b8bcae9067a19021ebf239f634b3e
奥村　凪 <k022g1055@g.neec.ac.jp>
2023-08-21 10:59:42 +0900

    ゲームタイトルを変更

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
 	win->CreateGameWindow(
-	    L"SKYSHOOTER", WS_OVERLAPPEDWINDOW & ~(WS_MAXIMIZEBOX | WS_THICKFRAME), 1280, 720);
+	    L"SCOREGETTER", WS_OVERLAPPEDWINDOW & ~(WS_MAXIMIZEBOX | WS_THICKFRAME), 1280, 720);
 	win->SetSizeChangeMode(WinApp::SizeChangeMode::kNone);
 
 	// DirectX初期化処理
