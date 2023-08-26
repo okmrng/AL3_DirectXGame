be9a58a0b163926ffbdef1de219db125c5f7f7c7
奥村　凪 <k022g1055@g.neec.ac.jp>
2023-08-26 09:26:44 +0900

    ゲーム説明シーンクラス作成

---
+++ Explanation.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "Input.h"
+#include "Model.h"
+#include "SafeDelete.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+
+/// <summary>
+/// ゲーム説明
+/// </summary>
+class Explanation {
+public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	Explanation();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~Explanation();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	void Draw();
+
+private:
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+};
